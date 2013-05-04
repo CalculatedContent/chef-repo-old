2233f6a1a7d780c9241e0bfd927ee12a87a5d37d
Charles H Martin <charlesmartin14@gmail.com>
2013-05-04 00:44:55 -0700

    .

--- roles/cloud_master.rb
+++ roles/cloud_master.rb
@@ -1,3 +1,3 @@
 name "cloud_master"
 description "Master node"
-run_list "recipe[apt]","recipe[build-essential]","recipe[git]","recipe[cloud-crawler::master]","recipe[chef-client]"
+run_list "recipe[apt]","recipe[build-essential]","recipe[git]","recipe[sinatra_app]","recipe[cloud-crawler::master]","recipe[chef-client]"
